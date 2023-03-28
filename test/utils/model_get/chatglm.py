527d8710ff6a0277364b8034a837fb9fe2c063eb
arslantu <arslantu@arslantu.xyz>
2023-03-28 15:59:15 +0800

    function to load and get chatglm model with and without finetuning

---
+++ test/utils/model_get/chatglm.py
@@ -0,0 +1,8 @@
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+model.eval()
+
+def get_chatglm():
+    return model, tokenizer
