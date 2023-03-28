285c8595eb3ce07a2b5893057b355bf9fc54fef8
arslantu <arslantu@arslantu.xyz>
2023-03-28 15:52:21 +0800

    move ask.py to utils folder

--- test/utils/ask.py
+++ test/utils/ask.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def ask_chatglm_6b(prompt, history):
+def ask(prompt, history):
     url = "http://127.0.0.1:8000"
     data = {"prompt": prompt, "history": history}
     res = requests.post(url, json.dumps(data)).json()
