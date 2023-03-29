c0aaf125dec859b53008d2849002edd8d0dda313
arslantu <arslantu@arslantu.xyz>
2023-03-29 14:52:19 +0800

    give up the unified api

--- test/utils/model_get/chatglm.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
-model.eval()
-
-def get_chatglm():
-    return model, tokenizer
